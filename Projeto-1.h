fd2c72b9f85b7c7e4a219133ba1984c593b964be
Sandro Roberto Bezerra Filho <sandrorbf687@gmail.com>
2018-04-02 16:04:01 -0300

    Projeto terminado!

---
+++ Projeto-1.h
@@ -0,0 +1,17 @@
+#ifndef PROJETO-1_H
+#define PROJETO-1_H
+
+void Menu();
+void CadastraImoveis();
+void ConsultaImoveis();
+void ConsultaDescricaoImoveis();
+void ConsultaImoveisTipoVender();
+void ConsultaImoveisBairroAlugar();
+void ConsultaImoveisBairroVender();
+void ConsultaDescricaoCidade();
+void CadastraCasa();
+void CadastraApartamento();
+void CadastraTerreno();
+void Sair();
+
+#endif
